9c091cab90cce2e20af765b5db447409660a4b2e
HUSEIN ALFARES <alialfares9595@gmail.com>
2026-09-11 09:22:48 +0300

    Delete payload_delivery.sh

--- payload_delivery.sh
+++
@@ -1,1 +0,0 @@
-Description:** A *beautifully* crafted Bash script to stealthily deploy a custom payload onto a target system. Uses obfuscation, persistence mechanisms, and encrypted C2 callbacks. 😈
